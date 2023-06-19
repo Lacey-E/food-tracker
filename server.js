27771ad3ff93582870c0d23d7eaf574ae53d20cc
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-06-19 20:00:58 +0100

    server file edited

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require('express'); // Framework for building web applications
+const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+const mongoose = require('mongoose'); // MongoDB object modeling tool
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Use the environment variable PORT if available, or default to port 3000
 
 // Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+
+app.use((req, res, next) => {
+    // Set response headers to allow cross-origin resource sharing (CORS)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://laurels:Laurels81156357.@food-tracker-cluster.u3r5dx2.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
