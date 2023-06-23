e4df41732dc05442e2b8499730ed542e9fe69cea
Ike-Ikon <asieduisaac5775@gmail.com>
2023-06-23 23:17:12 +0000

    swagger in route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.use('/', require('./swagger'));
 router.use('/user', require('./users'));
 
 router.use('/inventory', require('./inventory'));
