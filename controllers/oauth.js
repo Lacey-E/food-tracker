bcbdbe1ae3f0b554d34fe358c47c7053d7752738
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-07-17 19:38:58 +0100

    changes

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -6,37 +6,28 @@ const axios = require('axios');
 const session = require('express-session');
 
 app.use(session({
-    secret: process.env.GITHUB_CLIENT_SECRET,
-    resave:false,
-    saveUninitialized:true
-  }))
+  secret: process.env.GITHUB_CLIENT_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
 const login = async (req, res) => {
-    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&prompt=consent`)
-  };
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&prompt=consent`)
+};
 
-
-  const authenticated = (req,res,next) => {
-    try{
-        if (req.session.token){
-            next()
-        }else{
-            throw new Error("Please Login to Continue........")
-        }
-    }catch(err){
-        res.status(400).json({message:err.message})
+const authenticated = (req, res, next) => {
+  try {
+    if (req.session.token) {
+      next()
+    } else {
+      throw new Error("Please Login to Continue........")
     }
+  } catch (err) {
+    res.status(400).json({ message: err.message })
   }
-   
+}
 
-
-  
-
-
-
-
-
-  module.exports ={
-    login,
-authenticated
-  }
+module.exports = {
+  login,
+  authenticated
+}
--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -9,11 +9,6 @@ router.get('/', shopController.getAllShoppingLists);
 // GET a specific shopping list by ID
 router.get('/:id', shopController.getShoppingListById);
 
-// POST request to create a new shopping list
-router.post('/', shopController.createShoppingList);
-
-// Delete a shopping list by ID
-router.delete('/:id', shopController.deleteShoppingList);
 // POST request to create a new shop list
 router.post('/', oauthController.authenticated, shopController.createShoppingList);
 
